c10864cff5918731b92f75004048fbe32c47d78b
Chris Yeung <chris.yeung@shearwatersystems.com>
2016-10-27 14:39:19 +0100

    added render_slice method to prevent OOM

--- net_analysis.py
+++ net_analysis.py
@@ -28,7 +28,20 @@ def load_image(path, height=None, width=None):
     return skimage.transform.resize(img, (ny, nx))
 
 
-def view_layers(pb_path, img_path):
+def create_tiles(large, height, width, num):
+    h_stride = height / num
+    w_stride = width / num
+    t_tiles = []
+    for y in xrange(num):
+        row = []
+        for x in xrange(num):
+            t_tile = tf.slice(large, [0, y * h_stride, x * w_stride, 0], [1, h_stride, w_stride, 3])
+            row.append(t_tile)
+        t_tiles.append(row)
+    return t_tiles
+
+
+def render(pb_path, img_path):
     with tf.Session() as sess:
         img = load_image(img_path)
         contents = tf.expand_dims(tf.constant(img, tf.float32), 0)
@@ -51,12 +64,50 @@ def view_layers(pb_path, img_path):
         print "img saved:", out_path
 
 
+def render_sliced(pb_path, img_path, side_num):
+    with tf.Session() as sess:
+        img = load_image(img_path)
+        contents = tf.expand_dims(tf.constant(img, tf.float32), 0)
+
+        # use stitch training method, slice the image into tiles and concat as batches
+        tiles = create_tiles(contents, img.shape[0], img.shape[1], side_num)
+        batch = tf.concat(0, [tf.concat(0, tiles[y]) for y in xrange(side_num)])  # row1, row2, ...
+
+        net = TensorZoomNet(pb_path, False)
+        net.build(batch)
+
+        # stitch the tiles back together after split the batches
+        split = tf.split(0, side_num ** 2, net.output)
+        fast_output = tf.concat(1, [
+            tf.concat(2, [split[x] for x in xrange(side_num * y, side_num * y + side_num)])
+            for y in xrange(side_num)])
+
+        start_time = time.time()
+        output = sess.run(fast_output)
+        duration = time.time() - start_time
+        print "output calculated: %.10f sec" % duration
+
+        # print image
+        _, pb_name = os.path.split(pb_path)
+        pb_name, _ = os.path.splitext(pb_name)
+        name, ext = os.path.splitext(img_path)
+        out_path = name + "_" + pb_name + ext
+        skimage.io.imsave(out_path, output[0])
+        print "img saved:", out_path
+
+
 if __name__ == "__main__":
     # with tf.device("/gpu:0"):
     with tf.device("/cpu:0"):
         # for small image/ icon/ thumbnail, use non-deblur version has better result
-        # view_layers(pb_path='./results/tz6-s-stitch/tz6-s-stitch-gen.npy', img_path="./analysis/cat_h.jpg")
+        render(pb_path='./results/tz6-s-stitch/tz6-s-stitch-gen.npy', img_path="./analysis/cat_h.jpg")
 
-        # for large image/ photos from camera, deblur version looks better
-        view_layers(pb_path='./results/tz6-s-stitch-sblur-lowtv/tz6-s-stitch-sblur-lowtv-gen.npy',
-                    img_path="./analysis/london2.jpg")
+        # example for large image / photos from camera, deblur version looks better
+        # warning: this example will consume lots of memory (around 9.xxGB)
+        # render(pb_path='./results/tz6-s-stitch-sblur-lowtv/tz6-s-stitch-sblur-lowtv-gen.npy',
+        #             img_path="./analysis/london2.jpg")
+
+        # instead, slice the image into 4 smaller images and then join together to form a big one
+        # less memory is used (<1GB) but there will be defects on the boundary of the tiles
+        # render_sliced(pb_path='./results/tz6-s-stitch-sblur-lowtv/tz6-s-stitch-sblur-lowtv-gen.npy',
+        #               img_path="./analysis/london2.jpg", side_num=4)
